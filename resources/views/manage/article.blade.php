147424454b0200992ca0e0bf219150e127ef7410
saa-padae <brick2crosslink@gmail.com>
2023-02-15 14:52:47 +0900

    [fix] margin

--- resources/views/manage/article.blade.php
+++ resources/views/manage/article.blade.php
@@ -5,15 +5,15 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    @foreach ($qiita as $q)
+    @foreach ($qiita as $q)
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
                         <div>{{ $q->title }}</div>
-                    @endforeach
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endforeach
 </x-app-layout>
