65df271462dc57c5a6e5a64d05c0bfe3f3811528
saaa-padae <brick2crosslink@gmail.com>
2023-02-16 20:45:05 +0900

    [add]記事検索のroute

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\QiitaController;
 use App\Http\Controllers\MenteesController;
 use App\Http\Controllers\MypageController;
+use App\Http\Controllers\ArticleSeachController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,31 +22,23 @@ use App\Http\Controllers\MypageController;
 */
 
 Route::resource('manage', EvaluationController::class);
-
 Route::resource('permission', PermissionController::class);
-
-
 Route::resource('user', UserController::class);
-
-
-
-// Route::resource('qiita', QiitaController::class);
+Route::resource('mentees', MenteesController::class);
 
 Route::get('permission/{permission}/createNew',[PermissionController::class,'createNew'])->name('permission.createNew');
+Route::get('/article', [QiitaController::class, 'index'])->name('article');
+Route::get('/article', [ArticleSearchController::class, 'create'])->name('article');
+Route::get('/article/search', [ArticleSearchController::class, 'index'])->name('article.search');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/article', [QiitaController::class, 'index'])->name('article');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/article', function () {
-//     return view('manage.article');
-// })->name('article');
 
 Route::middleware('auth')->group(function () {
     Route::resource('mypage', MypageController::class);
@@ -54,8 +47,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('mypage/{mypage}/updateProfile', [MypageController::class, 'updateProfile'])->name('mypage.updateProfile');
     Route::patch('mypage/{mypage}/updateGithub', [MypageController::class, 'updateGithub'])->name('mypage.updateGithub');
 });
-
-Route::resource('mentees', MenteesController::class);
 
 
 Route::middleware('auth')->group(function () {
