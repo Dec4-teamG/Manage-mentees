9a86038c2289c61bd87d7091ba50c35583e113fd
soratooo <0116sorato@icloud.com>
2023-02-17 16:06:46 +0900

    [fix] article.blade.php

--- resources/views/manage/article.blade.php
+++ resources/views/manage/article.blade.php
@@ -30,8 +30,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-
-                        <div><a href="{{$q->url}}" target="_blank" rel="noopener noreferrer" class="underline text-gray-900 dark:text-white">{{ $q->title }}</a></div
+                        <a href="{{$q->url}}" target="_blank" rel="noopener noreferrer" class="underline">{{ $q->title }}</a>
                     </div>
                 </div>
             </div>
